218d290b0141cbd92d5633854c16e762cd804faf
rydservicio-bit <ryd.servicio@gmail.com>
2026-04-04 02:06:18 -0300

    Fix formatting of 'Acceso' link in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,7 @@ const navItems = [
   { label: "Arquitectura", href: "#arquitectura" },
   { label: "Soluciones", href: "#soluciones" },
   { label: "Contacto", href: "#contacto" },
-  { label: "Acceso", href: "#Acceso"
-  },
+  { label: "Acceso", href: "#Acceso"  },
 ];
 
 const Navbar = () => {
